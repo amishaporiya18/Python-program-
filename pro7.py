a914449eed8f318a2df1b3f49c36109cfe6d78f0
amishaporiya18 <amishaporiya1815@gmail.com>
2026-04-08 13:01:22 +0530

    Delete pro7.py

--- pro7.py
+++
@@ -1,6 +0,0 @@
-gender_counts = df['Gender'].value_counts()
-gender_counts.plot(kind='bar', color=['blue', 'pink'])
-plt.title('Male vs Female Students')
-plt.xlabel('Gender')
-plt.ylabel('Count')
-plt.show()
